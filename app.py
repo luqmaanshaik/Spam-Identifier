c95594a224602ae8ca8a4c5460e4fd677c46094a
luqmaanshaik <shaikluqmaan1@gmail.com>
2025-02-21 12:21:19 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-import streamlit as st
 
-st.title("Spam Identifier")
-st.write("Enter a message below to check if it's spam or not.")
-
-user_input = st.text_area("Message:")
-if st.button("Check"):
-    st.write("Spam detection model output will be displayed here.")
